ae913e4d5662f75143f11775a7e851a97fa83810
Alisson F. Fritzen <fritzen.alisson@Alissons-MacBook-Pro.local>
2022-03-16 19:38:49 -0300

    Teste de commit

---
+++ Aula2/Aula2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Aula2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime nasc = new DateTime(1993, 12, 30);
+            DateTime atual = DateTime.Today;
+
+            Console.WriteLine($"Idade: {calcularIdade(nasc, atual)}");
+        }
+
+        private static int calcularIdade(DateTime nasc, DateTime atual)
+        {
+            int dias = ((atual - nasc).Days) / 360;
+
+            return dias;
+            
+        }
+    }
+}
